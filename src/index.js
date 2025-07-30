5270465c96d295ed6902866728138a48c0295d8f
meriem <meriem.kaffela@esen.tn>
2025-07-30 19:06:48 +0200

    fix: load real homepage content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
